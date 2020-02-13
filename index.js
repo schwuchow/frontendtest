2589cec56b05561e517c571d84ed977d477cf5f0
Judali <jschwuchow@googlemail.com>
2020-02-13 22:19:34 +0100

    initial project version

---
+++ index.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    var gallery = document.querySelector('.gallery');
+
+    gallery.addEventListener('click', openImage, false);
+
+    function openImage(event) {
+        var imgSrc = event.target.src;
+        var imgTitle = event.target.alt;
+
+        var overlayString = 'overlay';
+        var overlay = document.querySelector(`.${overlayString}`);
+        var overlayImg = document.querySelector(`.${overlayString}__img`);
+        var overlayTitle = document.querySelector(`.${overlayString}__title`);
+
+        overlayImg.src = imgSrc;
+        overlayTitle.innerHTML = imgTitle;
+        
+        overlay.classList.add('openOverlay');
+
+        var closeBtn = document.querySelector('.close');
+
+        closeBtn.addEventListener('click', () => {
+            overlay.classList.remove('openOverlay');
+        });
+    }
+    
+});
