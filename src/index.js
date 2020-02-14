ebdd016264b3d250ac26e69f592852a91e5ed03d
Judali <jschwuchow@googlemail.com>
2020-02-14 16:39:30 +0100

    added subtitles + anim

--- src/index.js
+++ src/index.js
@@ -5,24 +5,27 @@ document.addEventListener('DOMContentLoaded', () => {
     gallery.addEventListener('click', openImage, false);
 
     function openImage(event) {
-        var imgSrc = event.target.src;
-        var imgTitle = event.target.alt;
 
-        var overlayString = 'overlay';
-        var overlay = document.querySelector(`.${overlayString}`);
-        var overlayImg = document.querySelector(`.${overlayString}__img`);
-        var overlayTitle = document.querySelector(`.${overlayString}__title`);
-
-        overlayImg.src = imgSrc;
-        overlayTitle.innerHTML = imgTitle;
-        
-        overlay.classList.add('openOverlay');
-
-        var closeBtn = document.querySelector('.close');
-
-        closeBtn.addEventListener('click', () => {
-            overlay.classList.remove('openOverlay');
-        });
+        if (event.target.src) {
+            var imgSrc = event.target.src;
+            var imgTitle = event.target.alt;
+    
+            var overlayString = 'overlay';
+            var overlay = document.querySelector(`.${overlayString}`);
+            var overlayImg = document.querySelector(`.${overlayString}__img`);
+            var overlayTitle = document.querySelector(`.${overlayString}__title`);
+    
+            overlayImg.src = imgSrc;
+            overlayTitle.innerHTML = imgTitle;
+            
+            overlay.classList.add('openOverlay');
+    
+            var closeBtn = document.querySelector('.close');
+    
+            closeBtn.addEventListener('click', () => {
+                overlay.classList.remove('openOverlay');
+            });
+        }
     }
     
 });
